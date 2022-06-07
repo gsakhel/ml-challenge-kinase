77b95cb14b42503ce9c878487637eaacd26f6b75
forest <livelyforest@protonmail.com>
2022-06-06 18:20:26 -0700

    Containerize

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update
+
+# install docker
+sudo apt-get install -y\
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# setup a docker container and run
+sudo docker build --no-cache --tag apptag .
+sudo docker run -d --name app -p 5000:8080 apptag
